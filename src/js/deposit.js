1521d63e63599c0805f5fb40b9d5a60a8103bc7f
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-05-18 19:41:47 +0600

    js update

--- src/js/deposit.js
+++ src/js/deposit.js
@@ -1,19 +1,18 @@
 document.getElementById("depositBtn").addEventListener("click", function () {
-    const inputDepositElement = document.getElementById("inputDeposit");
-    const inputDepositString= inputDepositElement.value;
-    const inputDeposit = parseFloat(inputDepositString);
+  const inputDepositElement = document.getElementById("inputDeposit");
+  const inputDepositString = inputDepositElement.value;
+  const inputDeposit = parseFloat(inputDepositString);
 
-    const depositStatusElement = document.getElementById("depositStatus");
-    const depositStatusString = depositStatusElement.innerText;
-    const depositStatus = parseFloat(depositStatusString);
-    
-    const newDepositStatus = depositStatus + inputDeposit;
-    depositStatusElement.innerText = newDepositStatus;
+  const depositStatusElement = document.getElementById("depositStatus");
+  const depositStatusString = depositStatusElement.innerText;
+  const depositStatus = parseFloat(depositStatusString);
 
+  const newDepositStatus = depositStatus + inputDeposit;
+  depositStatusElement.innerText = newDepositStatus;
 
-    const totalBalanceElement = document.getElementById("totalBalance");
-    const totalBalanceString = totalBalanceElement.innerText;
-    const totalBalance = parseFloat(totalBalanceString);
-    const newTotalBalance = totalBalance + inputDeposit;
-    totalBalanceElement.innerText = newTotalBalance;
+  const totalBalanceElement = document.getElementById("totalBalance");
+  const totalBalanceString = totalBalanceElement.innerText;
+  const totalBalance = parseFloat(totalBalanceString);
+  const newTotalBalance = totalBalance + inputDeposit;
+  totalBalanceElement.innerText = newTotalBalance;
 });

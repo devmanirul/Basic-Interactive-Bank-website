a64cd4981354f6a26e0acdd24cf996c6af181252
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-08-24 13:34:48 +0600

    update

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ document.getElementById("logInBtn").addEventListener("click", function () {
   const inputPassElement = document.getElementById("inputPass");
   const inputPass = inputPassElement.value;
 
-  if (inputEmail === "mmimonir1234@gmail.com" && inputPass === "123456") {
+  if (inputEmail === "mmimonir1234@gmail.com" && inputPass === "100499Ab@&") {
     window.location.href = "bank.html";
   } else {
     alert("Invalid Email or Password");
